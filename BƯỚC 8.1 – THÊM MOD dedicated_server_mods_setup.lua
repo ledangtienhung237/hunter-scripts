a060d3499970477eaf1cbe8a97d0e14b01e69888
ledangtienhung237 <154294569+ledangtienhung237@users.noreply.github.com>
2025-11-20 17:14:45 +0700

    Create BƯỚC 8.1 – THÊM MOD dedicated_server_mods_setup.lua

---
+++ BƯỚC 8.1 – THÊM MOD dedicated_server_mods_setup.lua
@@ -0,0 +1,6 @@
+cat > ~/dstserver/mods/dedicated_server_mods_setup.lua << 'EOF'
+ServerModSetup("378160973")     -- Global Positions
+ServerModSetup("2189004162")    -- Insight (Show Me+)
+ServerModSetup("1185229307")    -- Epic Healthbar
+ServerModSetup("501385076")     -- Quick Pick
+EOF
